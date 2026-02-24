f7e54a2d52432ef6033af5a9081ed2d830f3c1f4
hieuhp132 <hieuhp132@gmail.com>
2026-02-24 16:38:12 +0700

    ád

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -285,15 +285,15 @@ export default function HomePage() {
               <p className="platform-text" style={{color: '#ffffff'}}>Ant-Tech Asia - Candidate Referral Program</p>
               <p className="platform-text" style={{color: '#ffffff'}}>(Headhunter Referral Program)</p>
             </div>
-            <div className="testimonial">
-            <p className="quote" style={{color: '#ffffff'}}>
-              “Through Ant-Tech Asia’s program, I was able to earn additional income while helping my network find amazing job opportunities. The process is simple, and the commission is always transparent.”
-            </p>
-            <div className="author">
-              <FaStar className="author-icon" />
-              <span style={{color: '#ffffff'}}>Top Collaborator</span>
-            </div>
-          </div>
+            {/* <div className="testimonial">
+              <p className="quote" style={{color: '#ffffff'}}>
+                “Through Ant-Tech Asia’s program, I was able to earn additional income while helping my network find amazing job opportunities. The process is simple, and the commission is always transparent.”
+              </p>
+              <div className="author">
+                <FaStar className="author-icon" />
+                <span style={{color: '#ffffff'}}>Top Collaborator</span>
+              </div>
+            </div> */}
           </div>
           <div className="footer-divider1"></div>
           <div className="footer-bottom">
